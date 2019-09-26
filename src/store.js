1a2756b751ef678d0f9e2ec368d4f3ab8734c925
dg <dim.glynatsis@gmail.com>
2019-09-27 01:57:32 +0300

    - add electron builder

--- src/store.js
+++ src/store.js
@@ -5,11 +5,14 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    todos: [{
+      text: 'ela',
+    }],
   },
-  mutations: {
-
+  getters: {
+    allTodos: state => state.todos,
   },
+  mutations: {},
   actions: {
 
   },
